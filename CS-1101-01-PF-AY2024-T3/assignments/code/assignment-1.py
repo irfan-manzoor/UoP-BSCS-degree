147d5281073e4d91cdb122e0df8396ac807135a7
Arfan <rayazl@outlook.com>
2024-02-17 00:49:36 +0500

    Assignment 1 - use of variables and print function

---
+++ CS-1101-01-PF-AY2024-T3/assignments/code/assignment-1.py
@@ -0,0 +1,18 @@
+age = int(input("Enter your age: "))
+print("Age multiplied by 2 is:", age * 2)
+
+city = "Lahore"
+country = "Pakistan"
+continent = "Asia"
+print("You are living in {}, {}, {}".format(city, country, continent))
+
+exam_start_date = "28th March 2024"
+exam_end_date = "3rd April 2024"
+print("Examination Schedule: {} - {}".format(exam_start_date, exam_end_date))
+
+def ira(){
+    print('hello, world!')
+        
+    return 0
+}
+
