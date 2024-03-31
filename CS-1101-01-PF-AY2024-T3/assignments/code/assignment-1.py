06c80132a3bca565e52a8fba2d9d17dd194b262c
Irfan Manzoor <rayazl@outlook.com>
2024-04-01 01:33:15 +0500

    assignment-1 submitted

---
+++ CS-1101-01-PF-AY2024-T3/assignments/code/assignment-1.py
@@ -0,0 +1,11 @@
+age = int(input("Enter your age: ")) # Get user input as an integer
+print("Age multiplied by 2 is:", age * 2) # Print the result
+
+city = "Lahore"
+country = "Pakistan" 
+continent = "Asia"
+print("You are living in {}, {}, {}".format(city, country, continent)) 
+
+exam_start_date = "28th March 2024"
+exam_end_date = "3rd April 2024"
+print("Examination Schedule: {} - {}".format(exam_start_date, exam_end_date))
